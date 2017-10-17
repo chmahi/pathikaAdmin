fb072680743859051f42a1a0704e17d3e2b23322
dabidGleed <dabid@gleedtech.com>
2017-10-17 08:15:16 +0530

    Added toggle functionality

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,15 +1,72 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var jQuery: any;
+
 @Component({
   selector: 'header-component',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+        // Menu Toggle
+    jQuery('.toggle-btn').click(function() {
+        jQuery(".left-side").getNiceScroll().hide();
+
+        if (jQuery('body').hasClass('left-side-collapsed')) {
+            jQuery(".left-side").getNiceScroll().hide();
+        }
+        var body = jQuery('body');
+        var bodyposition = body.css('position');
+
+        if (bodyposition != 'relative') {
+
+            if (!body.hasClass('left-side-collapsed')) {
+                body.addClass('left-side-collapsed');
+                jQuery('.custom-nav ul').attr('style', '');
+
+                jQuery(this).addClass('menu-collapsed');
+
+            } else {
+                body.removeClass('left-side-collapsed chat-view');
+                jQuery('.custom-nav li.active ul').css({ display: 'block' });
+
+                jQuery(this).removeClass('menu-collapsed');
+
+            }
+        } else {
+
+            if (body.hasClass('left-side-show'))
+                body.removeClass('left-side-show');
+            else
+                body.addClass('left-side-show');
+
+            // mainContentHeightAdjust();
+        }
+
+    });
+
+   }
+
+   toggleMenu() {
+
+     if (jQuery('body').hasClass('left-side-collapsed')) {
+             jQuery('body').removeClass('left-side-collapsed')
+        } else {
+          jQuery('body').addClass('left-side-collapsed')
+        }
+
+         var body = jQuery('body');
+        var bodyposition = body.css('position');
+    
+   }
 
   ngOnInit() {
   }
+
+  
 
 }
